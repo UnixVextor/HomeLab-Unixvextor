2905779b90b2211686ad2712add6820a4f529a83
UnixVextor <withun.wch@gmail.com>
2024-04-14 20:58:16 +0700

    add: dns recored for srv-prod-2

--- dns/dns-bind9/server-ptr.tf
+++ dns/dns-bind9/server-ptr.tf
@@ -1,6 +1,13 @@
 resource "dns_ptr_record" "ptr_srv_1" {
   zone = "10.10.in-addr.arpa."
   name = "251.30"
-  ptr  = "srv-prod-1.unixvextor.com."
+  ptr  = "srv-prod-1.home.unixvextor.com."
+  ttl  = 300
+}
+
+resource "dns_ptr_record" "ptr_srv_2" {
+  zone = "10.10.in-addr.arpa."
+  name = "250.30"
+  ptr  = "srv-prod-2.home.unixvextor.com."
   ttl  = 300
 }
--- dns/dns-bind9/server.tf
+++ dns/dns-bind9/server.tf
@@ -11,3 +11,17 @@ resource "dns_a_record_set" "wild_srv_prod_1" {
   addresses = [ "10.10.30.251" ]
   ttl = 3600
 }
+
+resource "dns_a_record_set" "srv_prod_2" {
+  zone = "home.unixvextor.com."
+  name = "srv-prod-2"
+  addresses = [ "10.10.30.250" ]
+  ttl = 3600
+}
+
+resource "dns_a_record_set" "wild_srv_prod_2" {
+  zone = "home.unixvextor.com."
+  name = "*.srv-prod-2"
+  addresses = [ "10.10.30.250" ]
+  ttl = 3600  
+}
