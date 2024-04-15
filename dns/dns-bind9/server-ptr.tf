9d00a21732a7f013b42a07906ce0641f8c20afae
UnixVextor <withun.wch@gmail.com>
2024-04-15 23:16:38 +0700

    add: dns record srv_prod_3, srv_demo_1-3

--- dns/dns-bind9/server-ptr.tf
+++ dns/dns-bind9/server-ptr.tf
@@ -1,13 +1,39 @@
-resource "dns_ptr_record" "ptr_srv_1" {
+resource "dns_ptr_record" "ptr_srv_prod_1" {
   zone = "10.10.in-addr.arpa."
   name = "251.30"
   ptr  = "srv-prod-1.home.unixvextor.com."
   ttl  = 300
 }
 
-resource "dns_ptr_record" "ptr_srv_2" {
+resource "dns_ptr_record" "ptr_srv_prod_2" {
   zone = "10.10.in-addr.arpa."
   name = "250.30"
   ptr  = "srv-prod-2.home.unixvextor.com."
+  ttl  = 300
+}
+
+resource "dns_ptr_record" "ptr_srv_prod_3" {
+  zone = "10.10.in-addr.arpa."
+  name = "13.30"
+  ptr  = "srv-prod-3.home.unixvextor.com."
+  ttl  = 300
+}
+
+resource "dns_ptr_record" "ptr_srv_demo_1" {
+  zone = "10.10.in-addr.arpa."
+  name = "247.10"
+  ptr  = "srv-demo-1.home.unixvextor.com."
+  ttl  = 300
+}
+resource "dns_ptr_record" "ptr_srv_demo_2" {
+  zone = "10.10.in-addr.arpa."
+  name = "246.10"
+  ptr  = "srv-demo-2.home.unixvextor.com."
+  ttl  = 300
+}
+resource "dns_ptr_record" "ptr_srv_demo_3" {
+  zone = "10.10.in-addr.arpa."
+  name = "13.10"
+  ptr  = "srv-demo-3.home.unixvextor.com."
   ttl  = 300
 }
--- dns/dns-bind9/server.tf
+++ dns/dns-bind9/server.tf
@@ -19,9 +19,63 @@ resource "dns_a_record_set" "srv_prod_2" {
   ttl       = 3600
 }
 
-resource "dns_a_record_set" "wilds_srv_prod_2" {
+resource "dns_a_record_set" "wild_srv_prod_2" {
   zone      = "home.unixvextor.com."
   name      = "*.srv-prod-2"
   addresses = ["10.10.30.250"]
+  ttl       = 3600
+}
+
+resource "dns_a_record_set" "srv_prod_3" {
+  zone      = "home.unixvextor.com."
+  name      = "srv-prod-3"
+  addresses = ["10.10.30.13"]
+  ttl       = 3600
+}
+
+resource "dns_a_record_set" "wild_srv_prod_3" {
+  zone      = "home.unixvextor.com."
+  name      = "*.srv-prod-3"
+  addresses = ["10.10.30.13"]
+  ttl       = 3600
+}
+
+resource "dns_a_record_set" "srv_demo_1" {
+  zone      = "home.unixvextor.com."
+  name      = "srv-demo-1"
+  addresses = ["10.10.10.247"]
+  ttl       = 3600
+}
+
+resource "dns_a_record_set" "wild_srv_demo_1" {
+  zone      = "home.unixvextor.com."
+  name      = "*.srv-demo-1"
+  addresses = ["10.10.10.247"]
+  ttl       = 3600
+}
+resource "dns_a_record_set" "srv_demo_2" {
+  zone      = "home.unixvextor.com."
+  name      = "srv-demo-2"
+  addresses = ["10.10.10.246"]
+  ttl       = 3600
+}
+
+resource "dns_a_record_set" "wild_srv_demo_2" {
+  zone      = "home.unixvextor.com."
+  name      = "*.srv-demo-2"
+  addresses = ["10.10.10.246"]
+  ttl       = 3600
+}
+resource "dns_a_record_set" "srv_demo_3" {
+  zone      = "home.unixvextor.com."
+  name      = "srv-demo-3"
+  addresses = ["10.10.10.13"]
+  ttl       = 3600
+}
+
+resource "dns_a_record_set" "wild_srv_demo_3" {
+  zone      = "home.unixvextor.com."
+  name      = "*.srv-demo-3"
+  addresses = ["10.10.10.13"]
   ttl       = 3600
 }
