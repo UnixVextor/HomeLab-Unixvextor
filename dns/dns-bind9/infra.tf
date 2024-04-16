46897f6e75aff4bc58c4a94ed985665f1c7201ef
UnixVextor <withun.wch@gmail.com>
2024-04-17 00:27:32 +0700

    add: ip to point to treafik prod 1

--- dns/dns-bind9/infra.tf
+++ dns/dns-bind9/infra.tf
@@ -2,7 +2,8 @@ resource "dns_a_record_set" "proxmox" {
   zone = "home.unixvextor.com."
   name = "proxmox"
   addresses = [
-    "10.10.0.26"
+    "10.10.0.26",
+    "10.10.30.251"
   ]
   ttl = 3600
 }
