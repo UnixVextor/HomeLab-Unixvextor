3d1b6af69709049ec1d220c054425ebf293bfefa
UnixVextor <withun.wch@gmail.com>
2024-04-18 22:49:36 +0700

    delete: dns for truenas

--- dns/dns-bind9/infra.tf
+++ dns/dns-bind9/infra.tf
@@ -7,13 +7,3 @@ resource "dns_a_record_set" "proxmox" {
   ]
   ttl = 3600
 }
-
-resource "dns_a_record_set" "truenas" {
-  zone = "home.unixvextor.com."
-  name = "truenas"
-  addresses = [
-    "10.10.0.18",
-    "10.10.30.251"
-  ]
-  ttl = 3600
-}
--- dns/dns-bind9/server-ptr.tf
+++ dns/dns-bind9/server-ptr.tf
@@ -38,9 +38,3 @@ resource "dns_ptr_record" "ptr_srv_demo_3" {
   ttl  = 300
 }
 
-resource "dns_ptr_record" "ptr_proxmox" {
-  zone = "10.10.in-addr.arpa."
-  name = "26.0"
-  ptr  = "proxmox.home.unixvextor.com."
-  ttl  = 300
-}
