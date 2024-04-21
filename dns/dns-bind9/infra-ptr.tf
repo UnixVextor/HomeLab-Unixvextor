ab67a6bce35247bac6a792127ff5ddb118ff5fc4
unixvextor <withun.wch@gmail.com>
2024-04-21 17:03:56 +0700

    add: nas dns record

--- dns/dns-bind9/infra-ptr.tf
+++
@@ -1,6 +0,0 @@
-resource "dns_ptr_record" "ptr_proxmox" {
-  zone = "10.10.in-addr.arpa."
-  name = "26.0"
-  ptr  = "proxmox.home.unixvextor.com."
-  ttl  = 300
-}
--- dns/dns-bind9/infra.tf
+++ dns/dns-bind9/infra.tf
@@ -3,7 +3,15 @@ resource "dns_a_record_set" "proxmox" {
   name = "proxmox"
   addresses = [
     "10.10.0.26",
-    "10.10.30.251"
   ]
   ttl = 3600
 }
+
+resource "dns_a_record_set" "nas" {
+  zone = "home.unixvextor.com."
+  name = "nas"
+  addresses = [
+    "10.10.0.17",
+  ]
+  ttl = 3600
+} 
