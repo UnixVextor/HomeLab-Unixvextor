29aba5edd1d55dddee80e764004274991af9d26a
unixvextor <withun.wch@gmail.com>
2024-05-06 00:17:27 +0700

    add: dns record for srv-demo-4 and srv-prod-4

--- dns/dns-bind9/server.tf
+++ dns/dns-bind9/server.tf
@@ -39,7 +39,19 @@ resource "dns_a_record_set" "wild_srv_prod_3" {
   addresses = ["10.10.30.13"]
   ttl       = 3600
 }
+resource "dns_a_record_set" "srv_prod_4" {
+  zone      = "home.unixvextor.com."
+  name      = "srv-prod-4"
+  addresses = ["10.10.30.14"]
+  ttl       = 3600
+}
 
+resource "dns_a_record_set" "wild_srv_prod_4" {
+  zone      = "home.unixvextor.com."
+  name      = "*.srv-prod-4"
+  addresses = ["10.10.30.14"]
+  ttl       = 3600
+}
 resource "dns_a_record_set" "srv_demo_1" {
   zone      = "home.unixvextor.com."
   name      = "srv-demo-1"
@@ -77,5 +89,19 @@ resource "dns_a_record_set" "wild_srv_demo_3" {
   zone      = "home.unixvextor.com."
   name      = "*.srv-demo-3"
   addresses = ["10.10.10.13"]
+  ttl       = 3600
+}
+
+resource "dns_a_record_set" "srv_demo_4" {
+  zone      = "home.unixvextor.com."
+  name      = "srv-demo-4"
+  addresses = ["10.10.10.14"]
+  ttl       = 3600
+}
+
+resource "dns_a_record_set" "wild_srv_demo_4" {
+  zone      = "home.unixvextor.com."
+  name      = "*.srv-demo-4"
+  addresses = ["10.10.10.14"]
   ttl       = 3600
 }
