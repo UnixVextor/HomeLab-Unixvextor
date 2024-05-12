37108107f7e02a8862165e9a276d0f3821ef574b
unixvextor <withun.wch@gmail.com>
2024-05-12 12:34:25 +0700

    add: kubernetes prod cluster

---
+++ proxmox/terraform/ksrv-prod-1.tf
@@ -0,0 +1,56 @@
+resource "proxmox_vm_qemu" "krsv_prod_1" {
+  name        = "krsv-prod-1"
+  desc        = "-Kube node prod 1-"
+  agent       = 0
+  target_node = "prox"
+  qemu_os     = "other"
+  bios        = "seabios"
+  tags        = "demo,ubuntu-server,kube"
+
+
+  clone      = "ubuntu-template"
+  full_clone = true
+
+  # -- boot process -- 
+  onboot           = false
+  startup          = ""
+  automatic_reboot = true
+
+  cores   = 4
+  sockets = 1
+  memory  = 4096
+  cpu     = "host"
+
+
+  network {
+    model  = "virtio"
+    bridge = "vmbr10"
+  }
+
+  scsihw   = "virtio-scsi-pci" # default virtio-scsi-pci
+  bootdisk = "scsi0"
+
+  #   disk {
+  #     slot = 0
+  #     size      = "30G"
+  #     type      = "scsi"
+  #     storage   = "local-zfs"
+  #     cache     = "writeback"
+  #     discard   = "on"
+  #   }
+
+  lifecycle {
+    ignore_changes = [
+      disk,
+      vm_state
+    ]
+  }
+
+  # -- cloud-init --
+  ipconfig0  = "ip=10.10.30.31/24,gw=10.10.30.254"
+  nameserver = "10.10.30.249"
+  ciuser     = "unixvextor"
+  sshkeys    = <<EOF
+${var.PUBLIC_SSH_KEY}
+EOF
+}
