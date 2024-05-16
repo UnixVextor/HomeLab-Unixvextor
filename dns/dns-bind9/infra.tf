d7910afa59b65cc5c2e960406a844582a08443a6
unixvextor <withun.wch@gmail.com>
2024-05-16 23:38:06 +0700

    fix: send message

--- dns/dns-bind9/infra.tf
+++ dns/dns-bind9/infra.tf
@@ -16,7 +16,7 @@ resource "dns_a_record_set" "nas" {
   ttl = 3600
 }
 
-resource "dns_a_record_set" "pikvm" {
+resource "dns_a_record_set" "pi_kvm" {
   zone = "home.unixvextor.com."
   name = "pikvm"
   addresses = [
