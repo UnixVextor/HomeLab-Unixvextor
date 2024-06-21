9c0a6f50d2ff3d5ba265c02792bd08404e8a5c75
unixvextor <withun.wch@gmail.com>
2024-06-21 21:27:19 +0700

    update: terraform format for dns

--- dns/dns-bind9/server.tf
+++ dns/dns-bind9/server.tf
@@ -178,5 +178,5 @@ resource "dns_a_record_set" "antsrv_demo_1" {
   zone      = "home.unixvextor.com."
   name      = "antsrv-demo-1"
   addresses = ["10.10.10.41"]
-  ttl = 3600
+  ttl       = 3600
 }
