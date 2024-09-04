3eaef73daea5fe7f22d1fabf0bb8283d34607e35
unixvextor <withun.wch@gmail.com>
2024-09-04 18:46:47 +0700

    add: db-prod-2 dns record

--- dns/dns-bind9/app.tf
+++ dns/dns-bind9/app.tf
@@ -6,3 +6,11 @@ resource "dns_a_record_set" "db_prod_1" {
   ]
   ttl = 3600
 }
+resource "dns_a_record_set" "db_prod_2" {
+  zone = "home.unixvextor.com."
+  name = "db-prod-2"
+  addresses = [
+    "10.10.30.251"
+  ]
+  ttl = 3600
+}
